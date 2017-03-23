493d811e54af48273cd42c04a0f07960df92c0be
KongHyeontaek <rotanev7@gmail.com>
2017-03-23 21:44:26 +0900

    component 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // index.html의 <app-root></app-root> 안에 들어간다는 것
+  // templateUrl: './app.component.html',
+  template: `
+    <h1> inline Template</h1>
+    {{title}}
+    <app-other></app-other>
+    <app-another></app-another>
+  `,
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      color: red;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = 'app work!!!!';
 }
